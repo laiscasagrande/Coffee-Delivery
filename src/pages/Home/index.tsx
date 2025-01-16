9e11533d684baf0ca2ed1fcd83aa05e732f45575
laiscasagrande <laisluanaka@outlook.com>
2025-01-15 21:59:35 -0300

    :construction: Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,17 @@
 import imageCoffee from '../../assets/ImageCoffee.svg'
-export function Home(){
+import { ContentHome, ImageCoffee, TitleHome, TitleSubtitleHome } from './styles'
+export function Home() {
     return (
-       <>
-        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-        <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
-        <img src={imageCoffee} alt="Imagem de um café"/>
-       </>
+        <>
+            <ContentHome>
+                <TitleSubtitleHome>
+                    <TitleHome>Encontre o café perfeito para qualquer hora do dia</TitleHome>
+                    <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+                </TitleSubtitleHome>
+                <ImageCoffee src={imageCoffee} alt="Imagem de um café" />
+
+            </ContentHome>
+
+        </>
     )
 }
---
+++ src/pages/Home/styles.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+
+export const ContentHome = styled.main`
+width: 100vw;
+height: 34rem;
+background: #f0bdcd;
+display: flex;
+`
+
+export const TitleSubtitleHome = styled.section`
+display: flex;
+flex-direction: column;
+height: 12rem;
+width: 36.75rem;
+background: #009000
+`
+
+export const ImageCoffee = styled.img`
+width: 29.75rem;
+height: 22.5rem
+`
+
+export const TitleHome = styled.h1`
+font-family: "Baloo 2", serif;
+font-size: 3rem
+`
